ca5f46c9d41fba1eefbcede8d5241594c8cff2d7
LooperDog <[200~nz697448@gmail.com~>
2024-12-09 15:59:26 +0300

    The mystery has been solved

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,16 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+    @Autowired
+    private Deth8 death;
+    @Autowired
+    public void setDeath(Deth8 death) {
+        this.death = death;
+    }
 
     @Override
     public String toString() {
